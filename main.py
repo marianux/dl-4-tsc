9b3abbe9aa57374daeaaa4ff061d598c3a9846b9
Unknown <llamedom@gmail.com>
2018-12-12 12:02:21 -0300

    adapt mariano

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def create_classifier(classifier_name, input_shape, nb_classes, output_directory
 
 # change this directory for your machine
 # it should contain the archive folder containing both univariate and multivariate archives
-root_dir = '/mnt/nfs/casimir/'
+root_dir = '/home/mariano/mariano/misc/dl-4-tsc'
 
 if sys.argv[1]=='transform_mts_to_ucr_format':
     transform_mts_to_ucr_format()
